d808685f4e88251ce55e98952dd499e3fad8de29
Borja <borjaalvarez_18@hotmail.com>
2023-02-03 14:56:04 +0100

    lastest commits

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,47 +17,56 @@ export default function Hero() {
   return (
     <Box
       sx={{
-        backgroundImage: "linear-gradient(rgba(249, 197, 22, 0.7), rgba(249, 197, 22, 0.95)),url(/img/fondoweb.webp)",
-        backgroundColor: "#363f48",
+        backgroundImage: "url(/img/background.png)",
         backgroundSize: "cover",
-        backgroundAttachment: "scroll",
+        // backgroundAttachment: "scroll",
         backgroundPosition: "center",
-        display: "flex",
-        flexDirection: "column",
-        py: "100px",
+        // display: "flex",
+        // flexDirection: "column",
+        // py: "100px",
       }}
     >
-      <Container maxWidth='md'>
+      <Container maxWidth="md">
         <Grid container spacing={8}>
-          <Grid item xs={12} sm={12} md={6} sx={{ background: "transparent", display: "flex", justifyContent: "center" }}>
-            <img src='/logo_blanco.svg' alt='Vercel Logo' style={{ width: "100%", filter: "drop-shadow(3px 5px 12px rgb(0 0 0 / 0.3))" }} />
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            sx={{ background: "transparent", display: "flex", justifyContent: "center" }}
+          >
+            <img
+              src="/logo_blanco.svg"
+              alt="Vercel Logo"
+              style={{ width: "100%", filter: "drop-shadow(3px 5px 12px rgb(0 0 0 / 0.3))" }}
+            />
           </Grid>
 
           <Grid item xs={12} sm={12} md={6} sx={{ background: "white", backgroundColor: "transparent" }}>
             <Card sx={{ p: 1 }}>
               <CardActionArea>
-                <CardMedia component='img' height='300' image='/img/mandi.webp' alt='green iguana' />
+                <CardMedia component="img" height="300" image="/img/mandi.webp" alt="green iguana" />
                 <CardContent>
-                  <Typography gutterBottom variant='h5' component='div'>
+                  <Typography gutterBottom variant="h5" component="div">
                     Peluquería de mascotas
                   </Typography>
-                  <Typography variant='subtitle1' color='textSecondary'>
+                  <Typography variant="subtitle1" color="textSecondary">
                     Especialistas en el cuidado de tus mascotas.
                   </Typography>
 
-                  <Typography variant='subtitle1' color='textSecondary'>
+                  <Typography variant="subtitle1" color="textSecondary">
                     Lunes - Jueves: 09:30-13:30 y 15:30-19:30
                   </Typography>
-                  <Typography variant='subtitle1' color='textSecondary'>
+                  <Typography variant="subtitle1" color="textSecondary">
                     Viernes: 11:00-19:00
                   </Typography>
-                  <Typography variant='subtitle1' color='textSecondary'>
+                  <Typography variant="subtitle1" color="textSecondary">
                     Sabado: 09:30-13:30
                   </Typography>
                 </CardContent>
               </CardActionArea>
               <CardActions style={{ display: "flex", justifyContent: "center" }}>
-                <Button size='large' variant='contained' color='primary' startIcon={<CallIcon />}>
+                <Button size="large" variant="contained" color="primary" startIcon={<CallIcon />}>
                   Llamanos
                 </Button>
               </CardActions>
