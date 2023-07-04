1c2b262d80cec0e28ee1fee42e834820e4796174
Borja <borja.alvarez@hubbusiness.design>
2023-07-04 09:52:30 +0200

    new photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,11 +13,14 @@ import { useMediaQuery } from "@mui/material";
 import CallIcon from "@mui/icons-material/Call";
 
 export default function Hero() {
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("md")
+  );
   return (
     <Box
       sx={{
-        backgroundImage: "linear-gradient(rgba(249, 197, 22, 0.7), rgba(249, 197, 22, 0.95)),url(/img/fondoweb.webp)",
+        backgroundImage:
+          "linear-gradient(rgba(249, 197, 22, 0.7), rgba(249, 197, 22, 0.95)),url(/img/fondoweb.webp)",
         backgroundColor: "#363f48",
         backgroundSize: "cover",
         backgroundAttachment: "scroll",
@@ -34,19 +37,37 @@ export default function Hero() {
             xs={12}
             sm={12}
             md={6}
-            sx={{ background: "transparent", display: "flex", justifyContent: "center" }}
+            sx={{
+              background: "transparent",
+              display: "flex",
+              justifyContent: "center",
+            }}
           >
             <img
               src="/logo_blanco.svg"
               alt="o can de quen"
-              style={{ width: "100%", filter: "drop-shadow(3px 5px 12px rgb(0 0 0 / 0.3))" }}
+              style={{
+                width: "100%",
+                filter: "drop-shadow(3px 5px 12px rgb(0 0 0 / 0.3))",
+              }}
             />
           </Grid>
 
-          <Grid item xs={12} sm={12} md={6} sx={{ background: "white", backgroundColor: "transparent" }}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            sx={{ background: "white", backgroundColor: "transparent" }}
+          >
             <Card sx={{ p: 1 }}>
               <CardActionArea>
-                <CardMedia component="img" height="300" image="/img/plomo.jpg" alt="fot o de un perro" />
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="/img/kira2.png"
+                  alt="fot o de un perro"
+                />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     Peluquería de mascotas
@@ -66,8 +87,15 @@ export default function Hero() {
                   </Typography>
                 </CardContent>
               </CardActionArea>
-              <CardActions style={{ display: "flex", justifyContent: "center" }}>
-                <Button size="large" variant="contained" color="primary" startIcon={<CallIcon />}>
+              <CardActions
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <Button
+                  size="large"
+                  variant="contained"
+                  color="primary"
+                  startIcon={<CallIcon />}
+                >
                   Llamanos
                 </Button>
               </CardActions>
