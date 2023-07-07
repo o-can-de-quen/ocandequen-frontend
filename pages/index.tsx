5a0421251a8ccc015f16218b53c118485c9ef121
Borja <borja.alvarez@hubbusiness.design>
2023-07-07 09:33:04 +0200

    canonical link

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <link rel="canonical" href="https://ocandequen.es/" />
       </Head>
       <Appbar>
         <Hero />
